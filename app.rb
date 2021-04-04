8738d67873dfc903a7a763ad0db847f24de1d178
Jackie Sun <jackiesun2@gmail.com>
2021-04-05 09:56:53 +1000

    intialized programming concepts app

---
+++ app.rb
@@ -0,0 +1,49 @@
+# Home section
+def display_welcome
+    puts "*"*35
+    puts "Welcome."
+    puts "This is a Programming Concepts App."
+    puts "Press * to continue"
+    puts "*"*35
+end
+
+def display_menu
+    puts "*"*35
+    puts "1. Programming Concept Library"
+    puts "2. Add"
+    puts "3. Delete"
+    puts "4. Edit"
+    puts "*"*35
+end
+
+
+# display concept library
+def display_concept_library
+
+end
+
+
+# menu select
+
+def menu_select
+    #select display concept library
+    #select add
+    #select delete
+    #select edit
+end
+
+
+# add section
+
+def add_task
+    puts "Add Title"
+    title = gets.chomp
+    puts "Add Category:"
+    catergory = gets.chomp
+    puts "Add Diffculty"
+    difficulty = gets.chomp
+    puts "Add Description"
+    description = gets.chomp
+end 
+
+# 
