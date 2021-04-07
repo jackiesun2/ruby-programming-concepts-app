0cb8263b5d0976b560dc4d73fd26a0c788119182
Jackie Sun <jackiesun2@gmail.com>
2021-04-07 14:25:06 +1000

    added new ruby gem - TTY Prompt

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require "json"
+require "tty-prompt"
+
 
 class ConceptLibrary
     attr_accessor :concepts
@@ -37,6 +39,7 @@ class ConceptLibrary
             puts "Concept #{number}: #{concept[:name]} | Language Category: #{concept[:category]}"
             number += 1
         end
+        puts "*"*35
     end
 
     def display_concept(concept)
@@ -53,24 +56,46 @@ class ConceptLibrary
 
 # menu input
 
+    # def menu_input(input)
+    #     case input
+    #     when 0 
+    #         display_concept_library
+    #         puts "Select the concept you would like to view"
+    #         select_concept(validate_input_number)
+    #         menu_return
+    #     when 1 
+    #         add_concept
+    #     when 2
+    #         puts "Select the concept number you would like to delete"
+    #         delete_concept
+    #     when 3
+    #         edit_concept
+    #     when 4
+    #         exit
+    #     end 
+    # end
+
     def menu_input(input)
+        choices = {Elo: 1, Add: 2, Delete: 3, Edit: 4, Exit: 5}
+        prompt.select("What size?", choices)
         case input
-        when 0 
+        when 1 
             display_concept_library
             puts "Select the concept you would like to view"
-            select_concept
+            select_concept(validate_input_number)
             menu_return
-        when 1 
+        when 2 
             add_concept
-        when 2
+        when 3
             puts "Select the concept number you would like to delete"
             delete_concept
-        when 3
-            edit_concept
         when 4
+            edit_concept
+        when 5
             exit
         end 
     end
+
 
     def menu_return
         puts "Press enter to return to menu"
@@ -104,23 +129,22 @@ class ConceptLibrary
     end
 
     def load_concepts
-        data = File.read("app.json")
-        JSON.parse(data, symbolize_names: true)
+        begin
+            data = File.read("app.json")
+            JSON.parse(data, symbolize_names: true)
+        rescue Errno::ENOENT
+            puts "This file does not exist, please make the file"
+            exit
+        end 
     end
 
 # select concept
-
-    def select_concept
-        concept_number = (gets.chomp.to_i - 1)
-        display_concept(@concepts[concept_number])
-    end
 
     def select_concept_edit
         concept_number = (gets.chomp.to_i - 1)
         display_concept(@concepts[concept_number])
         @concepts[concept_number]
     end
-
 
 # delete concept
 
@@ -162,10 +186,44 @@ class ConceptLibrary
         elsif edit == "e"
             puts "New Explanation:"
             concept[:explained] = gets.chomp
+        else
+            puts 
         end
     end
 
+# error handling 
+
+    def validate_input_number
+        #regex to solve this? 
+        #multiple number? 
+        #how to fix this to input a letter to get an error?
+        loop do
+            concept_number = gets.chomp.to_i * 1
+                if concept_number >= 1
+                    concept_number = (concept_number - 1)
+                        if @concepts.length >= (concept_number + 1)
+                            return concept_number
+                        else
+                            puts "Number is not between 1 - #{@concepts.length} try another number"
+                        end
+                else 
+                    # puts "This is not a number, Press enter to try again"
+                    # gets
+                end
+                puts "This is not a number, Press enter to try again"
+                gets
+            end
+    end
+        
+
+    def select_concept(index)
+        display_concept(@concepts[index])
+    end
+
+# initialize new 
+
     new = ConceptLibrary.new
     new.display_welcome
+    prompt = TTY::Prompt.new
 
-end
+end 
